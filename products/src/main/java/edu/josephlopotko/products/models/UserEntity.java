0979ac9b99a42cdbe09e324b539cdc5a8890d4c7
Joseph Lopotko <142354212+mrjoebling@users.noreply.github.com>
2026-01-22 15:38:49 -0700

    commit JPA annotations for UserEntity

--- products/src/main/java/edu/josephlopotko/products/models/UserEntity.java
+++ products/src/main/java/edu/josephlopotko/products/models/UserEntity.java
@@ -1,5 +1,10 @@
 package edu.josephlopotko.products.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class UserEntity {
 
 }
