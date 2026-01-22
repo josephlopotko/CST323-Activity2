49f78a7c5ad695ac82cfaa63454a71ea618480a7
Joseph Lopotko <142354212+mrjoebling@users.noreply.github.com>
2026-01-22 15:40:17 -0700

    Commit ID

--- products/src/main/java/edu/josephlopotko/products/models/UserEntity.java
+++ products/src/main/java/edu/josephlopotko/products/models/UserEntity.java
@@ -1,5 +1,8 @@
 package edu.josephlopotko.products.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
@@ -7,4 +10,7 @@ import jakarta.persistence.Table;
 @Table(name = "users")
 public class UserEntity {
 
+    @Id
+    @Column
+    private int id;
 }
