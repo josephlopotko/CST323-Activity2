b589cd00cbdc0a5fda3d81ce73b348d46766a7a4
Joseph Lopotko <142354212+mrjoebling@users.noreply.github.com>
2026-01-22 20:35:43 -0700

    Add UsersRepository using Spring Data JPA

--- products/src/main/java/edu/josephlopotko/products/data/UsersRepository.java
+++ products/src/main/java/edu/josephlopotko/products/data/UsersRepository.java
@@ -2,7 +2,10 @@ package edu.josephlopotko.products.data;
 
 import org.springframework.data.repository.CrudRepository;
 import edu.josephlopotko.products.models.UserEntity;
+import java.util.List;
+
 
 public interface UsersRepository extends CrudRepository<UserEntity, Integer>{
 
+    UserEntity findByUsername(String username);
 }
